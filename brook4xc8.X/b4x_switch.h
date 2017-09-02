7a56a07fe7e7940b2e931e02f697dacb0863c8b8
silvioprog <silvioprog@gmail.com>
2017-09-02 14:00:13 -0300

    Added initial macros for switch handling.

---
+++ brook4xc8.X/b4x_switch.h
@@ -0,0 +1,81 @@
+/*    _____   _____    _____   _____   _   __
+ *   |  _  \ |  _  \  /  _  \ /  _  \ | | / /
+ *   | |_) | | |_) |  | | | | | | | | | |/ /
+ *   |  _ <  |  _ <   | | | | | | | | |   (
+ *   | |_) | | | \ \  | |_| | | |_| | | |\ \
+ *   |_____/ |_|  \_\ \_____/ \_____/ |_| \_\ 4-XC8.
+ *
+ *   –– a small XC8 library for development with 8-bit PIC MCUs.
+ *
+ * Copyright (c) 2017 Silvio Clecio <silvioprog@gmail.com>
+ *
+ * This file is part of Brook4-XC8.
+ *
+ * Brook4-XC8 is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Brook4-XC8 is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Brook4-XC8.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef B4X_SWITCH_H
+#define	B4X_SWITCH_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "b4x_types.h"
+
+/**
+ * Maps switch clicking.
+ * @param p Port bit.
+ */
+#ifndef B4X_SW_CLICK
+#define B4X_SW_CLICK(p) \
+    static B4X_VBIT B4X_SW_##p##_CLICKED; \
+    if (!(p)) \
+        B4X_SW_##p##_CLICKED = 0; \
+    if (((p) && !B4X_SW_##p##_CLICKED))
+#endif
+
+/**
+ * Maps switch clicking in pull-up style.
+ * @param p Port bit.
+ */
+#ifndef B4X_SW_nCLICK
+#define B4X_SW_nCLICK(p) \
+    static B4X_VBIT B4X_SW_##p##_CLICKED; \
+    if ((p)) \
+        B4X_SW_##p##_CLICKED = 0; \
+    if (((!p) && !B4X_SW_##p##_CLICKED))
+#endif
+
+/**
+ * Avoid switch bouncing on #B4X_SW_CLICK or #B4X_SW_nCLICK calls.
+ * @param p Port bit.
+ */
+#ifndef B4X_SW_DEBOUNCE
+#define B4X_SW_DEBOUNCE(p) B4X_SW_##p##_CLICKED = 1;
+#endif
+
+/* TODO: longclick? */
+
+/* TODO: toggle? */
+
+/* TODO: longtoggle? */
+
+/* TODO: pulse? */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
